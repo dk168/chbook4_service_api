30211191e1e18996ecca7af9e1cb4b448a998f26
dengkui <123456>
2022-04-26 15:04:46 +0800

    初始化

---
+++ src/JdClientServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dk
+ * Date: 2022/4/26
+ * Time: 10:50
+ * Features:
+ */
+
+namespace App\JsonRpc;
+
+
+interface JdClientServiceInterface {
+    public function queryBooks(array $params): array;
+
+    /**
+     * 查询
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function skuSearchBooks(array $params): array;
+}
