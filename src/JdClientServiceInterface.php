816b7598609a8309438ce6fedaba34fa452d108a
dengkui <123456>
2022-05-09 13:57:56 +0800

    更新京东服务接口

--- src/JdClientServiceInterface.php
+++ src/JdClientServiceInterface.php
@@ -11,14 +11,57 @@ namespace Chbook4\ServiceApi;
 
 
 interface JdClientServiceInterface {
-    public function queryBooks(array $params): array;
-
     /**
-     * 查询
+     * 图书商品全量查询接口
      *
      * @param array $params
      *
      * @return array
      */
     public function skuSearchBooks(array $params): array;
+
+    /**
+     * 查询商品详情
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function batchGetSkuInfo(array $params): array;
+
+    /**
+     * 查询图书音像基本信息
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function queryBookVideoBasicField(array $params): array;
+
+    /**
+     * 图书大字段接口查询
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function queryBookVideoBigField(array $params): array;
+
+    /**
+     * 查询图书图片
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function queryImage(array $params): array;
+
+    /**
+     * 查询图书价格
+     *
+     * @param string $sku_id
+     *
+     * @return array
+     */
+    public function getPrice(string $sku_id): array;
 }
